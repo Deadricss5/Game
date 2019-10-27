95aad44cd9c192be1b85fb15d0ae6a4211aac5d5
Sergey <man.reg@yandex.ru>
2019-10-27 09:33:57 +0200

    add render box function

--- index.js
+++ index.js
@@ -7,5 +7,20 @@ $start.addEventListener('click', startGame)
 function startGame() {
     $game.style.backgroundColor = '#fff';
     $start.classList.add('hide');
-    
+
+    renderBox()
+}
+
+
+function renderBox() {
+    let box = document.createElement('div')
+
+    box.style.height = box.style.width = '50px'
+    box.style.position = 'absolute'
+    box.style.backgroundColor = '#000'
+    box.style.top = '50px'
+    box.style.left = '70px'
+    box.style.cursor = 'pointer'
+
+    $game.insertAdjacentElement("afterbegin", box)
 }
