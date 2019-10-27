dceedf1fdbb8118397e99b8d0c6af210efe63d21
Sergey <man.reg@yandex.ru>
2019-10-27 09:25:01 +0200

    removed the start button and changed the background at the start of the game

---
+++ index.js
@@ -0,0 +1,11 @@
+let $start  = document.querySelector('#start')
+let $game   = document.querySelector('#game')
+
+$start.addEventListener('click', startGame)
+
+
+function startGame() {
+    $game.style.backgroundColor = '#fff';
+    $start.classList.add('hide');
+    
+}
