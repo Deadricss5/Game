22a3989345c31b5fd4cfe610735fe40ab1a1df46
Sergey <man.reg@yandex.ru>
2019-11-10 11:58:30 +0200

    add timer and endGame function'

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 let $start  = document.querySelector('#start')
 let $game   = document.querySelector('#game')
+let $time = document.querySelector('#time')
 let score  = 0
+let isGameStarted = false
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
 
 
 function startGame() {
+    isGameStarted = true
     $game.style.backgroundColor = '#fff';
     $start.classList.add('hide');
+
+    let interval = setInterval(function () {
+        let time = parseFloat($time.textContent)
+
+        if(time <= 0) {
+            clearInterval(interval)
+            endGame() 
+        }else {
+            $time.textContent = (time - 0.1).toFixed(1)
+        }
+    }, 100)
 
     renderBox()
 }
 
+function endGame() {
+    isGameStarted = false
+}
+
 function handleBoxClick() {
+    if (!isGameStarted) {
+        return
+    }
     if (event.target.dataset.box) {
         score++
         renderBox()
@@ -42,3 +63,5 @@ function renderBox() {
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
+
+
