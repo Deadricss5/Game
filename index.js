8f231a6530b8a9f992697b8c3e54c2890cd474ce
Sergey <man.reg@yandex.ru>
2019-11-10 12:30:29 +0200

    refactoring

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ let $time = document.querySelector('#time')
 let $result = document.querySelector('#result')
 let $timeHeader = document.querySelector('#time-header')
 let $resultHeader = document.querySelector('#result-header')
+let $gameTime = document.querySelector('#game-time')
 
 let score  = 0
 let isGameStarted = false
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
+$gameTime.addEventListener('input', setGameTime)
 
+function show($el) {
+    $el.classList.remove('hide')
+
+}
+
+function hide($el) {
+    $el.classList.add('hide')
+}
 
 function startGame() {
     score = 0
-    $timeHeader.classList.remove('hide')
-    $resultHeader.classList.add('hide')
+    $gameTime.setAttribute('disabled', true)
     isGameStarted = true
     $game.style.backgroundColor = '#fff';
-    $start.classList.add('hide');
+    hide($start)
 
     let interval = setInterval(function () {
         let time = parseFloat($time.textContent)
@@ -40,18 +49,21 @@ function setGameScore(){
 }
 
 function setGameTime() {
-    let time = 5
+    let time = +$gameTime.value
     $time.textContent = time.toFixed(1)
+    show($timeHeader)
+    hide($resultHeader)
 }
 
 function endGame() {
     isGameStarted = false
     setGameScore()
-    $start.classList.remove('hide')
+    show($start)
     $game.style.backgroundColor = '#ccc'
     $game.innerHTML = ''
-    $timeHeader.classList.add('hide')
-    $resultHeader.classList.remove('hide')
+    hide($timeHeader)
+    show($resultHeader)
+    $gameTime.removeAttribute('disabled')
     
 }
 
@@ -75,7 +87,7 @@ function renderBox() {
 
     box.style.height = box.style.width = boxSize + 'px'
     box.style.position = 'absolute'
-    box.style.backgroundColor = '#000'
+    box.style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 256 - 0) + ',' + Math.floor(Math.random() * 256 - 0) + ',' + Math.floor(Math.random() * 256 - 0) + ')'
     box.style.top = getRandom(0,maxTop) + 'px'
     box.style.left = getRandom(0,maxLeft) + 'px'
     box.style.cursor = 'pointer'
