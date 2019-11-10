25d78e1b967545cbf394f83ab7234bd0d0450f21
Sergey <man.reg@yandex.ru>
2019-11-10 10:55:17 +0200

    add random square render function'
    s
    exit
    exit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function startGame() {
 }
 
 function handleBoxClick() {
-    if (event.target.dataset) {
+    if (event.target.dataset.box) {
         score++
         renderBox()
     }
@@ -23,14 +23,22 @@ function handleBoxClick() {
 function renderBox() {
     $game.innerHTML = ''
     let box = document.createElement('div')
+    let boxSize = getRandom(30,100)
+    let gameSize = $game.getBoundingClientRect()
+    let maxTop = gameSize.height - boxSize
+    let maxLeft = gameSize.width - boxSize
 
-    box.style.height = box.style.width = '50px'
+    box.style.height = box.style.width = boxSize + 'px'
     box.style.position = 'absolute'
     box.style.backgroundColor = '#000'
-    box.style.top = '50px'
-    box.style.left = '70px'
+    box.style.top = getRandom(0,maxTop) + 'px'
+    box.style.left = getRandom(0,maxLeft) + 'px'
     box.style.cursor = 'pointer'
     box.setAttribute('data-box', true)
 
     $game.insertAdjacentElement("afterbegin", box)
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
 }
